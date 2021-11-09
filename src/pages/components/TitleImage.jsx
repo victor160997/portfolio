0add5d3d39a7a09464bf397a67a683107fb7601d
Victor Mendonca <victor.men.dev@gmail.com>
2021-11-09 17:45:14 -0300

    atualizando a capa

--- src/pages/components/TitleImage.jsx
+++ src/pages/components/TitleImage.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom';
 import Sidebar from './Sidebar';
 import './TitleImage.css';
 
@@ -8,9 +7,11 @@ export default class TitleImage extends Component {
     return (
       <div className="title-image-body animationDown">
         <div className="back-image">
-          <Sidebar />
-          <img src="https://media-exp1.licdn.com/dms/image/C4E03AQERYDcvf4FHYw/profile-displayphoto-shrink_800_800/0/1625498861613?e=1637193600&v=beta&t=rIVTC0AQ7A30O3XqHKNk713FthL_dGC1OeoCxb2FndI" alt="foto do victor" />
-          <h1>Hi, my name is <i className="my-name">Victor! </i> I'm a Front End developer, <b className="my-name">come see my work!</b></h1>
+          <div className="gradient">
+            <Sidebar />
+            <img src="https://media-exp1.licdn.com/dms/image/C4E03AQERYDcvf4FHYw/profile-displayphoto-shrink_800_800/0/1625498861613?e=1637193600&v=beta&t=rIVTC0AQ7A30O3XqHKNk713FthL_dGC1OeoCxb2FndI" alt="foto do victor" />
+            <h1>Hi, my name is <i className="my-name">Victor! </i> I'm a Front End developer, <b className="my-name">come see my work!</b></h1>
+          </div>
         </div>
       </div>
     )
